57428d6f70e7862553f0d77463fa25b55370d3ab
Richard Acquaye <racquaye89@gmail.com>
2023-04-05 16:53:45 +0100

    Store bookmarked recipe

--- routes/cookbook.js
+++ routes/cookbook.js
@@ -40,6 +40,45 @@ router.get("/", (req, res) => {
 });
 
 // BOOKMARK RECIPE
+router.post("/", (req, res) => {
+  const cookbook = JSON.parse(fs.readFileSync(bookmarkedRecipes));
+  const recipeId = req.body.id;
+  const recipeTitle = req.body.title;
+  const recipeAuthor = req.body.sourceName;
+  const recipeImage = req.body.image;
+
+  const recipeInfo = {
+    id: recipeId,
+    title: recipeTitle,
+    sourceName: recipeAuthor,
+    image: recipeImage,
+  };
+
+  //CHECK IF THE RECIPE ALREADY EXISTS
+  const recipeCheck = cookbook.find((recipe) => recipe.id === recipeId);
+  console.log(recipeCheck);
+  //if it does, return a response that it exists
+  if (recipeCheck) {
+    return res.status(409).json({
+      error: true,
+      message: `${recipeTitle} already exists in your cookbook`,
+    });
+  }
+  cookbook.push(recipeInfo);
+
+  fs.writeFile(bookmarkedRecipes, JSON.stringify(cookbook), (err) => {
+    if (err) {
+      return res.status(500).json({
+        error: true,
+        message: `Could not store ${recipeTitle}`,
+      });
+    }
+    return res.status(201).json({
+      error: false,
+      message: `${recipeTitle} saved successfully`,
+    });
+  });
+});
 
 // REMOVE BOOKMARKED RECIPE
 
