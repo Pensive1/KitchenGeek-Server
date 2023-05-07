f432d4435c32dc92f5e336a752904a23c85b81ba
Richard Acquaye <racquaye89@gmail.com>
2023-05-07 03:59:09 +0100

    Add Delete user  bookmark from db function

--- routes/cookbook.js
+++ routes/cookbook.js
@@ -1,15 +1,12 @@
 const router = require("express").Router();
-const fs = require("fs");
-// const { default: knex } = require("knex");
 const knex = require("knex")(require("../knexfile.js"));
-const bookmarkedRecipes = "./storage/user/cookbook.json";
-const cookbook = JSON.parse(fs.readFileSync(bookmarkedRecipes));
+const userId = 1; // <-- Refactor "1" as user id
 
-const bookmarkCheck = (userId, recipeId) => {
+const bookmarkCheck = (user_id, recipeId) => {
   return knex("recipe_bookmarks")
     .select("*")
     .from("recipe_bookmarks")
-    .where("user_id", userId)
+    .where("user_id", user_id)
     .andWhere("recipe_id", recipeId)
     .then((data) => {
       return data.length > 0;
@@ -22,7 +19,7 @@ const bookmarkCheck = (userId, recipeId) => {
 // GET (USER) BOOKMARKED RECIPES
 router.get("/", (req, res) => {
   knex("recipe_bookmarks")
-    .where("user_id", 1) // <-- Refactor for OAuth
+    .where("user_id", userId) // <-- Refactor for OAuth
     .then((data) => {
       res.json(data);
     })
@@ -35,7 +32,7 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   const recipeId = Number(req.params.id);
 
-  bookmarkCheck(1, recipeId) // <-- Refactor "1" as user id
+  bookmarkCheck(userId, recipeId)
     .then((result) => {
       if (result) {
         return res
@@ -54,7 +51,6 @@ router.get("/:id", (req, res) => {
 
 // BOOKMARK RECIPE
 router.post("/", (req, res) => {
-  const userId = 1; // <-- Refactor "1" as user id param
   const recipeId = req.body.recipe_id; // <-- Double check the request data in the client. Match this with it
   const recipeTitle = req.body.recipe_title;
   const recipeAuthor = req.body.recipe_author;
@@ -98,27 +94,52 @@ router.post("/", (req, res) => {
 // REMOVE BOOKMARKED RECIPE
 router.delete("/:id", (req, res) => {
   const recipeId = Number(req.params.id);
-  const targetRecipe = cookbook.findIndex((recipe) => recipe.id === recipeId);
-  console.log(targetRecipe);
-  const recipeTitle = targetRecipe ? cookbook[targetRecipe].title : null;
+  // const targetRecipe = cookbook.findIndex((recipe) => recipe.id === recipeId);
+  // console.log(targetRecipe);
+  // const recipeTitle = targetRecipe ? cookbook[targetRecipe].title : null;
 
-  if (targetRecipe === -1) {
-    return res.status(404).json({ message: "This recipe doesn't exist" });
-  }
+  // if (targetRecipe === -1) {
+  //   return res.status(404).json({ message: "This recipe doesn't exist" });
+  // }
 
-  cookbook.pop(cookbook[targetRecipe]);
-  fs.writeFile(bookmarkedRecipes, JSON.stringify(cookbook), (err) => {
-    if (err) {
-      return res
-        .status(500)
-        .json({ error: true, message: "Could not update records" });
-    }
+  // cookbook.pop(cookbook[targetRecipe]);
+  // fs.writeFile(bookmarkedRecipes, JSON.stringify(cookbook), (err) => {
+  //   if (err) {
+  //     return res
+  //       .status(500)
+  //       .json({ error: true, message: "Could not update records" });
+  //   }
 
-    return res.status(200).json({
-      error: false,
-      message: "Recipe removed",
-      title: recipeTitle,
+  //   return res.status(200).json({
+  //     error: false,
+  //     message: "Recipe removed",
+  //     title: recipeTitle,
+  //   });
+  // });
+
+  bookmarkCheck(userId, recipeId)
+    .then((result) => {
+      if (result) {
+        knex("recipe_bookmarks")
+          .where("user_id", userId)
+          .andWhere("recipe_id", recipeId)
+          .del()
+          .then((data) => {
+            return res.status(204).json({
+              error: false,
+              message: "Recipe removed",
+            });
+          });
+      } else {
+        return res.status(404).json({ message: "This recipe doesn't exist" });
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({
+        error: true,
+        message: `Could not update records: ${err.message}`,
+      });
     });
-  });
 });
 module.exports = router;
